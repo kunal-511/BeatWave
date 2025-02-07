e5fb5f7347a547e6867ea53d4ac39260a043eb7a
kunal Dugar <yoyokvunal@gmail.com>
2025-02-07 19:45:28 +0530

    Complete Project

---
+++ frontend/src/components/Topbar.tsx
@@ -0,0 +1,52 @@
+import { SignedOut, UserButton } from "@clerk/clerk-react";
+import { LayoutDashboardIcon } from "lucide-react";
+import { Link } from "react-router-dom";
+import SignInOAuthButtons from "./SignInOAuthButtons";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
+import { useNavigate } from "react-router-dom";
+
+const Topbar = () => {
+  const { isAdmin } = useAuthStore();
+  console.log({ isAdmin });
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 
+      backdrop-blur-md z-10
+    "
+    >
+      <div
+        className="flex gap-2 items-center cursor-pointer"
+        onClick={() => navigate("/")}
+      >
+        <img
+          src="https://alternative.me/media/256/beatwave-icon-8x42bk54wwlw7535-c.png"
+          alt="BeatWave Logo"
+          className="size-8"
+        />
+        BeatWave
+      </div>
+      <div className="flex items-center gap-4">
+        {isAdmin && (
+          <Link
+            to={"/admin"}
+            className={cn(buttonVariants({ variant: "outline" }))}
+          >
+            <LayoutDashboardIcon className="size-4  mr-2" />
+            Admin Dashboard
+          </Link>
+        )}
+
+        <SignedOut>
+          <SignInOAuthButtons />
+        </SignedOut>
+
+        <UserButton />
+      </div>
+    </div>
+  );
+};
+export default Topbar;
---
+++ frontend/src/pages/admin/components/Header.tsx
@@ -0,0 +1,24 @@
+import { UserButton } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-3 mb-8">
+        <Link to="/" className="rounded-lg flex items-center gap-2 ">
+          <img
+            src="https://alternative.me/media/256/beatwave-icon-8x42bk54wwlw7535-c.png"
+            className="size-10 text-black"
+          />
+          <p className="text-2xl"> BeatWave</p>
+        </Link>
+        <div>
+          <h1 className="text-3xl font-bold">Music Manager</h1>
+          <p className="text-zinc-400 mt-1">Manage your music catalog</p>
+        </div>
+      </div>
+      <UserButton />
+    </div>
+  );
+};
+export default Header;
