3ef26e134f52dfacacdecd2643412a430d3393cd
kunal-511 <yoyokvunal@gmail.com>
2025-08-27 19:48:51 +0530

    add redis for albums
    Signed-off-by: kunal-511 <yoyokvunal@gmail.com>

--- backend/src/controller/album.controller.js
+++ backend/src/controller/album.controller.js
@@ -1,9 +1,21 @@
 import { Album } from "../models/album.model.js";
+import client from "../lib/redis.js";
 
 export const getAllAlbums = async (req, res, next) => {
+
+	const cacheKey = 'all_albums';
+	const cachedData = await client.get(cacheKey);
+	if (cachedData) {
+		console.log("from cache");
+		return res.json(JSON.parse(cachedData));
+	}
+
+
 	try {
 		const albums = await Album.find();
 		res.status(200).json(albums);
+		await client.setEx(cacheKey, 1200, JSON.stringify(albums)); 
+
 	} catch (error) {
 		next(error);
 	}
@@ -12,11 +24,18 @@ export const getAllAlbums = async (req, res, next) => {
 export const getAlbumById = async (req, res, next) => {
 	try {
 		const { albumId } = req.params;
-		if(!albumId) {
+		if (!albumId) {
 			return res.status(400).json({ message: "Album ID is required" });
+		}
+		const cacheKey = `album:${albumId}`;
+		const cachedData = await client.get(cacheKey);
+		if (cachedData) {
+			console.log("from cache");
+			return res.json(JSON.parse(cachedData));
 		}
 
 		const album = await Album.findById(albumId).populate("songs");
+		await client.setEx(cacheKey, 1200, JSON.stringify(album));
 
 		if (!album) {
 			return res.status(404).json({ message: "Album not found" });
